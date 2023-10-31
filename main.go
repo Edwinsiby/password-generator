5dcc646b5950f46c1c849357c1046b0d0486bc5b
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-10-31 14:28:10 +0530

    added handlers

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"pass/helper"
+	"pass/handler"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,31 +16,7 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	router.POST("/generate", func(c *gin.Context) {
-
-		length := 12
-		if lenStr := c.PostForm("length"); lenStr != "" {
-			fmt.Sscanf(lenStr, "%d", &length)
-		}
-
-		includeUppercase := c.PostForm("includeUppercase") == "on"
-		includeLowercase := c.PostForm("includeLowercase") == "on"
-		includeNumbers := c.PostForm("includeNumbers") == "on"
-		includeSpecialChars := c.PostForm("includeSpecialChars") == "on"
-
-		if includeUppercase != true {
-			includeUppercase = true
-		}
-		if includeSpecialChars != true {
-			includeSpecialChars = true
-		}
-
-		password := helper.GeneratePassword(length, includeUppercase, includeLowercase, includeNumbers, includeSpecialChars)
-
-		c.HTML(http.StatusOK, "generated_password.html", gin.H{
-			"password": password,
-		})
-	})
+	router.POST("/generate", handler.Generate)
 
 	router.Run(":8080")
 }
